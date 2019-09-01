95f3c2faf38bf83964c3d58b91d056aacd4f3f52
Andrew Johnson <akj55@drexel.edu>
2019-09-01 16:57:20 -0400

    Final Push
    Website is fully completed, sorted, and ready to be turned in.

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -149,3 +149,20 @@ $("nav[data-nav='main-navigation'] a").on("click", openCloseMenu);
 $("#close-btn").on("click", openCloseMenu);
 // close the mobile menu when the overlay link is tapped / click
 $("#overlay").on("click", openCloseMenu);
+
+
+// collapsible
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
